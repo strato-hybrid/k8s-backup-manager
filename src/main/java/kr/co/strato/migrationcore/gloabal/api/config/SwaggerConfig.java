63fa67723e693ad1fd6368ecf2de46269c42699d
strato-cyj <strato128b@strato.co.kr>
2023-11-13 21:20:03 +0900

    스웨거 config 설정 추가

---
+++ src/main/java/kr/co/strato/migrationcore/gloabal/api/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package kr.co.strato.migrationcore.gloabal.api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Migration Core API")
+                .description("Migration Core API")
+                .version("1.0.0");
+    }
+}
