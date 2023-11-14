fc4f5d260d6c256effd47bd3e3c4d36470562fd8
strato-kyj <strato128f@strato.co.kr>
2023-11-14 18:45:34 +0900

    query dsl 설정 추가

---
+++ src/main/java/kr/co/strato/migrationcore/MigrationCoreApplication.java
@@ -0,0 +1,13 @@
+package kr.co.strato.migrationcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationCoreApplication.class, args);
+	}
+
+}
